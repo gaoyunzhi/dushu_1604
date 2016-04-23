89b82e7d5870762156e67bb05bfdaf5ad37a52e8
Yunzhi Gao <gaoyunzhi@gmail.com>
2016-04-23 07:12:41 -0700

    commit

--- client/scripts/controllers/register.controller.js
+++ client/scripts/controllers/register.controller.js
@@ -1,8 +1,10 @@
 angular
   .module('Root')
   .controller('RegisterCtrl', RegisterCtrl);
+
  
 function RegisterCtrl($scope, $reactive, $state, $stateParams, $ionicLoading, $ionicPopup, $log) {
+  MIN_DIFFERENT_CHAR = 30;
   $reactive(this).attach($scope);
  
   this.register = register;
@@ -14,25 +16,29 @@ function RegisterCtrl($scope, $reactive, $state, $stateParams, $ionicLoading, $i
     if (this.password_retype != this.password) {
       return handleError({reason: 'Password does not match'}); 
     }
-    if (!this.name || !this.wechat_id) {
-      return handleError({reason: 'WeChat ID and name is required'});
+    if (!this.name || !this.topic || !this.goal || !this.intro) {
+      return handleError({reason: 'All fields are required'});
+    }
+    intro = generate_intro(this);
+    different_char = (new Set(intro.split(''))).size
+    if (different_char < MIN_DIFFERENT_CHAR) {
+      return handleError({reason: '我们是一个深度交流的平台，请完善自我介绍'});
     }
 
     Accounts.createUser(
       {
-        username: this.email, 
+        username: this.name, 
         password: this.password, 
-        profile: {
-          name: this.name,
-          wechat_id: this.wechat_id,
-        }
+        email: this.email,
+        topic: this.topic,
+        goal: this.goal,
+        intro: this.intro,
       }, 
       (err) => {
         if (err) {
           return handleError(err);
         }
-        Meteor.call('welcome');
-        $state.go('tab.chat');
+        $state.go('chat');
       }
     );
   }
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,26 +1,17 @@
 Meteor.startup(function () {
   if (Meteor.users.find(
-    {username: Meteor.settings.admin.email}
+    {email: Meteor.settings.admin.email}
   ).count() == 0) {
     result = Accounts.createUser({
-      username: Meteor.settings.admin.email,
+      email: Meteor.settings.admin.email,
       password: Meteor.settings.admin.password,
-      profile: {
-        name: '阿云',
-        wechat_id: 'fenxiangdushu',
-      }
-    });
-    Accounts.createUser({
-      username: Meteor.settings.crawler.email,
-      password: Meteor.settings.crawler.password,
-      profile: {
-        name: '阿云看豆瓣',
-        wechat_id: 'fenxiangdushu'
-      }
+      username: '阿云',
+      topic: '读书，人生',
+      goal: '交朋友',
+      intro: '上海人，程序员，喜爱技术，也爱读书。最近迷茫中，正在探索人生的意义。'
     });
   }
-  ADMIN = Meteor.users.findOne({username: Meteor.settings.admin.email});
-  CRAWLER = Meteor.users.findOne({username: Meteor.settings.crawler.email});
+  ADMIN = Meteor.users.findOne({email: Meteor.settings.admin.email});
   if (AdminID.find().count() == 0) {
     AdminID.insert({admin_id: ADMIN._id});
   }  
