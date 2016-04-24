3eb2f0efbd0449aa0ca643fd41e7ede393ada0da
Yunzhi Gao <gaoyunzhi@gmail.com>
2016-04-24 11:20:00 -0700

    commit

--- client/scripts/controllers/profile.controller.js
+++ client/scripts/controllers/profile.controller.js
@@ -6,6 +6,7 @@ function ProfileCtrl($scope, $reactive, $stateParams, $ionicScrollDelegate, $tim
   $reactive(this).attach($scope);
   this.logout = logout;
   $scope.profile = {};
+  $scope.needSaveRating = false;
 
   Meteor.subscribe('users');
   Meteor.subscribe('reviews');
@@ -14,21 +15,34 @@ function ProfileCtrl($scope, $reactive, $stateParams, $ionicScrollDelegate, $tim
 
   update = function() {
     $scope.profile = Meteor.users.findOne({_id: $stateParams.id});
+    if (!$scope.profile) {
+      return;
+    }
+    $scope.profile.chatHref = "#/room/0/" + $scope.profile._id;
   };
 
   $scope.rating = {};
   $scope.rating.rate = 3;
-  $scope.rating.max = 5;
 
-  $scope.ratingsCallback = function(rating) {
-    console.log('Selected rating is : ', rating);
-  };
+  $scope.$watchCollection('rating.detail', (newVal, oldVal) => {
+    $scope.needSaveRating = true;
+  });
+
+  $scope.$watchCollection('rating.rate', (newVal, oldVal) => {
+    changeRating();
+  });
+
 
   function logout() {
     Meteor.logout((err) => {
       if (err) return; 
       $state.go('login');
     });
+  }
+
+  function changeRating() {
+    console.log("changeRating");
+    $scope.needSaveRating = false;
   }
 
   Tracker.autorun(function() {
